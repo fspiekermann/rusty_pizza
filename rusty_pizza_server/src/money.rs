5199a70ac331efdd8c0bcfbc35a52e1a8a699560
Björn Lange <blange@itemis.com>
2021-02-05 13:39:15 +0000

    Simple money construction

--- rusty_pizza_server/src/money.rs
+++ rusty_pizza_server/src/money.rs
@@ -1,10 +1,15 @@
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Money {
-
+    before_decimal_point: u32,
+    after_decimal_point: u8,
 }
 
 impl Money {
     fn new(before_decimal_point: u32, after_decimal_point: u8) -> Money {
-        Money { }
+        Money {
+            before_decimal_point: 5,
+            after_decimal_point: 50,
+        }
     }
 }
 
@@ -17,5 +22,11 @@ mod tests {
     fn money_can_be_created() {
         // When:
         let money = Money::new(5, 50);
+
+        // Then:
+        assert_eq!(money, Money {
+            before_decimal_point: 5,
+            after_decimal_point: 50,
+        })
     }
 }
