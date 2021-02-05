954d9e760e1e28320f2429a0c7431d371fd4264a
Björn Lange <blange@itemis.com>
2021-02-05 09:19:35 +0000

    Introduce Meals aggregation for multiple ordered meals

--- rusty_pizza_server/src/main.rs
+++ rusty_pizza_server/src/main.rs
@@ -1,17 +1,22 @@
 use std::rc::Rc;
 
 // TODO: calculate change money
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Meal {
     /// Number of the meal in the menu
     meal_id: String,
     /// Size of the pizza or noodle type etc.
     variety: String,
-    owner: Rc<User>,
     price: f64,
+    specials: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Meals {
+    meals: Vec<Meal>,
+    owner: Rc<User>,
     paid: f64,
     tip: f64,
-    specials: Vec<String>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -22,9 +27,9 @@ enum OrderStatus {
     Delivered,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Order {
-    meals: Vec<Meal>,
+    meals: Vec<Meals>,
     status: OrderStatus,
     manager: Rc<User>,
 }
