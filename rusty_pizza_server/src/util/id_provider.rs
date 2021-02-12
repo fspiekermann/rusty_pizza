af65eb2d0a0c4e8303188fb75b2fbf73b207755c
Björn Lange <blange@itemis.com>
2021-02-12 09:54:54 +0000

    Second ID is one

--- rusty_pizza_server/src/util/id_provider.rs
+++ rusty_pizza_server/src/util/id_provider.rs
@@ -1,12 +1,16 @@
-pub struct IdProvider {}
+pub struct IdProvider {
+    next_id: u32,
+}
 
 impl IdProvider {
     fn new() -> IdProvider {
-        IdProvider {}
+        IdProvider { next_id: 0 }
     }
 
     fn generate_next(&mut self) -> u32 {
-        0
+        let next = self.next_id;
+        self.next_id = next + 1;
+        next
     }
 }
 
@@ -24,5 +28,18 @@ mod tests {
 
         // Then:
         assert_eq!(id, 0);
+    }
+
+    #[test]
+    fn second_id_is_one() {
+        // Given:
+        let mut id_provider = IdProvider::new();
+        id_provider.generate_next();
+
+        // When:
+        let id = id_provider.generate_next();
+
+        // Then:
+        assert_eq!(id, 1);
     }
 }
