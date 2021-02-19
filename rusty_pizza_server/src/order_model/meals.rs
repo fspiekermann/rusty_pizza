d2afbc698ecb4b6553d974e74b8d18cf0b0ed61f
fspiekermann <fspiekermann@itemis.de>
2021-02-19 17:06:45 +0100

    #3 add test change_is_calculated_correctly

--- rusty_pizza_server/src/order_model/meals.rs
+++ rusty_pizza_server/src/order_model/meals.rs
@@ -32,6 +32,14 @@ impl Meals {
         self.meals.get_mut(&id).unwrap()
     }
 
+    pub fn set_paid(&mut self, paid: Money) {
+        self.paid = paid;
+    }
+
+    pub fn set_tip(&mut self, tip: Money) {
+        self.tip = tip;
+    }
+
     pub fn calculate_total_price(&self) -> Money {
         let mut total_price = Money::new(0,0);
         for (_, meal) in self.meals.iter() {
@@ -125,19 +133,41 @@ mod tests {
         case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money::new(15, 8)),
         case(vec![Money::new(3, 50), Money::new(4, 42)], Money::new(7, 92)),
     )]
-    fn total_price_is_calculated_corectly(prices: Vec<Money>, expected_total: Money) {
+    fn total_price_is_calculated_correctly(prices: Vec<Money>, expected_total: Money) {
         //Given
         let user = Rc::new(User::new(String::from("Peter")));
         let mut meals = Meals::new(user.clone());
         let mut meal_factory = MealFactory::new();
 
         for price in prices.into_iter() {
-            let meal = meal_factory.create_meal(String::from("03"), String::from("groß"), price);
+            let meal = meal_factory.create_meal(String::from("XX"), String::from("something"), price);
             meals.add_meal(meal);
         }
         //When
         let calculated_total = meals.calculate_total_price();
         //Then
-        assert_eq!(expected_total, calculated_total)
+        assert_eq!(expected_total, calculated_total);
+    }
+
+    #[rstest(prices, paid, tip, expected_change,
+        case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money::new(20, 0), Money::new(2, 2), Money::new(2, 0)),
+        case(vec![Money::new(3, 50), Money::new(4, 42)], Money::new(10, 50), Money::new(1, 50), Money::new(1, 8)),
+    )]
+    fn change_is_calculated_correctly(prices: Vec<Money>, paid: Money, tip: Money, expected_change: Money) {
+        //Given
+        let user = Rc::new(User::new(String::from("Peter")));
+        let mut meals = Meals::new(user.clone());
+        meals.set_paid(paid);
+        meals.set_tip(tip);
+        let mut meal_factory = MealFactory::new();
+
+        for price in prices.into_iter() {
+            let meal = meal_factory.create_meal(String::from("XX"), String::from("something"), price);
+            meals.add_meal(meal);
+        }
+        //When
+        let calculated_change = meals.calculate_change();
+        //Then
+        assert_eq!(expected_change, calculated_change);
     }
 }
