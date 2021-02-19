69bbb94edd6d4031a31edd4c31c30f8504597b5d
fspiekermann <fspiekermann@itemis.de>
2021-02-19 16:32:05 +0100

    #3 test for calculate total price of meals

--- rusty_pizza_server/src/order_model/meals.rs
+++ rusty_pizza_server/src/order_model/meals.rs
@@ -31,11 +31,23 @@ impl Meals {
         self.meals.insert(id, meal);
         self.meals.get_mut(&id).unwrap()
     }
+
+    // pub fn calculate_total_price(self) -> Money {
+    //     let mut total_price = Money::new(0,0)
+    //     for (meal, quantity) in self.meals.iter() {
+
+    //     }
+    // }
+
+    // pub fn calculate_change(self) -> Money {
+
+    // }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn meals_can_be_created() {
@@ -105,5 +117,23 @@ mod tests {
                 tip: Money::new(0, 0),
             }
         );
+    }
+
+    #[rstest(prices, expected_total,
+        case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money { cents: 1508 }),
+        case(vec![Money::new(3, 50), Money::new(4, 42)], Money { cents: 792 }),
+    )]
+    fn total_price_is_calculated_corectly(prices: Vec<Money>, expected_total: Money) {
+        //Given
+        let user = Rc::new(User::new(String::from("Peter")));
+        let mut meals = Meals::new(user.clone());
+
+        for price in prices.into_iter() {
+            meals.add_meal(Meal::new(String::from("03"), String::from("groß"), price));
+        }
+        //When
+        let calculated_total = meals.calculate_total_price();
+        //Then
+        assert_eq!(expected_total, calculated_total)
     }
 }
