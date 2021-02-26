767c93f8b30b5f03e2bf889bafd598101870c1f3
Fabio Spiekermann <fspiekermann@itemis.de>
2021-02-26 15:12:57 +0100

    Feature/3 calculate change money (#11)
    * #3 test for calculate total price of meals
    
    * #3: calculate_total_price
    
    * #3 add test change_is_calculated_correctly
    
    * #3: implement calculate_change
    
    * #3 auto formatting
    
    * #3 Adjust review
    
    * implement ChangeMoneyError
    
    * #3 shorten assert
    
    * #3 fixing typo

--- rusty_pizza_server/src/order_model/meals.rs
+++ rusty_pizza_server/src/order_model/meals.rs
@@ -2,7 +2,30 @@ use crate::order_model::meal::Meal;
 use crate::order_model::user::User;
 use crate::util::money::Money;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 use std::rc::Rc;
+
+#[derive(Debug, PartialEq)]
+pub enum ChangeMoneyError {
+    Underpaid(Money), // Contains the difference between paid money and has to pay 
+}
+
+impl fmt::Display for ChangeMoneyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use ChangeMoneyError::*;
+        match *self {
+            Underpaid(missing) => write!(
+                f,
+                "You have underpaid by {},{}Euro ",
+                missing.get_euros(),
+                missing.get_cents()
+            ),
+        }
+    }
+}
+
+impl Error for ChangeMoneyError {}
 
 #[derive(Debug, PartialEq)]
 pub struct Meals {
@@ -31,11 +54,37 @@ impl Meals {
         self.meals.insert(id, meal);
         self.meals.get_mut(&id).unwrap()
     }
+
+    pub fn set_paid(&mut self, paid: Money) {
+        self.paid = paid;
+    }
+
+    pub fn set_tip(&mut self, tip: Money) {
+        self.tip = tip;
+    }
+
+    pub fn calculate_total_price(&self) -> Money {
+        let mut total_price = Money::new(0, 0);
+        for meal in self.meals.values() {
+            total_price = total_price + meal.get_price();
+        }
+        return total_price;
+    }
+
+    pub fn calculate_change(&self) -> Result<Money, ChangeMoneyError> {
+        let has_to_pay = self.calculate_total_price() + self.tip;
+        if self.paid.get_total_cents() < has_to_pay.get_total_cents() {
+            return Err(ChangeMoneyError::Underpaid(has_to_pay - self.paid));
+        }
+        return Ok(self.paid - has_to_pay);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::order_model::meal::MealFactory;
+    use rstest::rstest;
 
     #[test]
     fn meals_can_be_created() {
@@ -105,5 +154,82 @@ mod tests {
                 tip: Money::new(0, 0),
             }
         );
+    }
+
+    #[rstest(prices, expected_total,
+        case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money::new(15, 08)),
+        case(vec![Money::new(3, 50), Money::new(4, 42)], Money::new(7, 92)),
+    )]
+    fn total_price_is_calculated_correctly(prices: Vec<Money>, expected_total: Money) {
+        //Given
+        let user = Rc::new(User::new(String::from("Peter")));
+        let mut meals = Meals::new(user);
+        let mut meal_factory = MealFactory::new();
+
+        for price in prices.into_iter() {
+            let meal =
+                meal_factory.create_meal(String::from("XX"), String::from("something"), price);
+            meals.add_meal(meal);
+        }
+        //When
+        let calculated_total = meals.calculate_total_price();
+        //Then
+        assert_eq!(expected_total, calculated_total);
+    }
+
+    #[rstest(prices, paid, tip, expected_change,
+        case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money::new(20, 0), Money::new(2, 20), Money::new(2, 72)),
+        case(vec![Money::new(3, 50), Money::new(4, 42)], Money::new(10, 50), Money::new(1, 50), Money::new(1, 8)),
+    )]
+    fn positve_change_is_calculated_correctly(
+        prices: Vec<Money>,
+        paid: Money,
+        tip: Money,
+        expected_change: Money,
+    ) {
+        //Given
+        let user = Rc::new(User::new(String::from("Peter")));
+        let mut meals = Meals::new(user);
+        meals.set_paid(paid);
+        meals.set_tip(tip);
+        let mut meal_factory = MealFactory::new();
+
+        for price in prices.into_iter() {
+            let meal =
+                meal_factory.create_meal(String::from("XX"), String::from("something"), price);
+            meals.add_meal(meal);
+        }
+        //When
+        let calculated_change = meals.calculate_change().unwrap();
+        //Then
+        assert_eq!(expected_change, calculated_change);
+    }
+
+    #[rstest(prices, paid, tip, expected_change,
+        case(vec![Money::new(2, 25), Money::new(5, 50), Money::new(7, 33)], Money::new(15, 0), Money::new(2, 20), ChangeMoneyError::Underpaid(Money::new(2, 28))),
+        case(vec![Money::new(3, 50), Money::new(4, 42)], Money::new(7, 50), Money::new(1, 50), ChangeMoneyError::Underpaid(Money::new(1, 92))),
+    )]
+    fn negative_change_returns_underpaid(
+        prices: Vec<Money>,
+        paid: Money,
+        tip: Money,
+        expected_change: ChangeMoneyError,
+    ) {
+        //Given
+        let user = Rc::new(User::new(String::from("Peter")));
+        let mut meals = Meals::new(user);
+        meals.set_paid(paid);
+        meals.set_tip(tip);
+        let mut meal_factory = MealFactory::new();
+
+        for price in prices.into_iter() {
+            let meal =
+                meal_factory.create_meal(String::from("XX"), String::from("something"), price);
+            meals.add_meal(meal);
+        }
+        //When
+        let change = meals.calculate_change();
+        //Then
+        assert_eq!(Err(expected_change), change);
     }
 }
