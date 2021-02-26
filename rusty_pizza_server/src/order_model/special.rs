5875ba61dee697d7579c29899abe0317a5ba66bf
Björn Lange <blange@itemis.com>
2021-02-26 10:24:28 +0000

    #15 Special can be created with ID and description

--- rusty_pizza_server/src/order_model/special.rs
+++ rusty_pizza_server/src/order_model/special.rs
@@ -1,5 +1,31 @@
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Special {
+    id: u32,
+    description: String,
+}
+
+impl Special {
+    pub fn new(id: u32, description: String) -> Special {
+        Special { id, description }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn special_can_be_created() {
+        // When:
+        let special = Special::new(0, String::from("Käserand"));
+
+        // Then:
+        assert_eq!(
+            special,
+            Special {
+                id: 0,
+                description: String::from("Käserand")
+            }
+        );
+    }
 }
